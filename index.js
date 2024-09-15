c3188fe50687c1ae5cac1c2ca177ddba1aa5308d
Gabriel Fabro <gabriel_fabro@hotmail.com>
2024-09-14 22:22:06 -0300

    Fundamentos 002

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+// arrays, objetos
 
 let meta = {
     value: 'ler um livro todo mês',
-    checked: true,
+    addres: 2,
+    checked: false,
+    isChecked: (info) => {
+        console.log(info)
+    }
 }
 
-let metas = [
-    meta,
-    {
-        value: "caminhar 20 minutos todos os dias",
-        checked: false
-    }
-]
+meta.value = "não é mais ler um livro"
+meta.isChecked(meta.value)
 
-console.log(metas[0].value)
+//function // arrow functions
+
+const criarMeta = () => {
+
+}
