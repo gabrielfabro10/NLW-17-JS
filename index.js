cd74b1ea4002e8074b5c147cf82c5a333ff72f76
Gabriel Fabro <gabriel_fabro@hotmail.com>
2024-09-15 22:48:44 -0300

    remover metas

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const metasRealizadas = async () => {
     }
 
     await select ({
-        message: "Metas realizadas " + realizadas.length,
+        message: "Metas realizadas:" + realizadas.length,
         choices: [...realizadas]
     })
 }
@@ -74,8 +74,32 @@ const metasAbertas = async () => {
     }
 
     await select({
-        message: "Metas Abertas" + abertas.length,
+        message: "Metas Abertas:" + abertas.length,
         choices: [...abertas]
+    })
+}
+
+const deletarMetas = async () => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return { value: meta.value, checked: false}
+    })
+
+    const itensADeletar = await checkbox({
+        message: "Selecione item para deletar",
+        choices: [...metasDesmarcadas],
+        instructions: false,
+    })
+
+
+    if(itensADeletar.length == 0){
+        console.log("Nenhum item para deletar!")
+        return
+    }
+
+    itensADeletar.forEach((item) => {
+        metas.filter((meta) => {
+            return meta.value != item
+        })
     })
 }
 
@@ -103,6 +127,10 @@ const start = async () => {
                     value: "abertas"
                 },
                 {
+                    name: "Deletar Metas",
+                    value: "deletar"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -123,6 +151,9 @@ const start = async () => {
                 break
             case "abertas":
                 await metasAbertas()
+                break
+            case "deletar":
+                await deletarMetas()
                 break
             case "sair":
                 console.log("Até a próxima!")
